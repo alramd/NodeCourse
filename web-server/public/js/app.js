138a443995a92c9e50b77c5b9e18980f71d2cd04
Jordan Coates <jordancoates4@outlook.com>
2022-04-08 11:38:28 +1000

    ignore modules

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -15,7 +15,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     messageOne.textContent = 'Loading...'
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if(data.error){
                 messageOne.textContent = data.error
@@ -24,7 +24,6 @@ weatherForm.addEventListener('submit', (e) => {
                 messageOne.textContent =  data.location
                 messageTwo.textContent =  data.forecast
             }
-
         })
     })
 })
